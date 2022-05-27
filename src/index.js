da761620fb27b2a2d7d4a8b13d8bb5ee2ec2ecb4
mgorem <oremgitonga22@gmail.com>
2022-05-27 14:23:31 +0300

    change to react 18 conventional standard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // We only call it once in the entire application
 
  import App from './App';
- ReactDOM.render(
-     <App />, document.getElementById('root')
- );
+
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ /**
+  * React DOM is used to render our components and entire application
+  * into the real DOM i.e into a DIV with an id of root
+  * 
+  */
