1529e98d56811a26eef4cc4125d9f57f98d797b0
mgorem <oremgitonga22@gmail.com>
2022-05-28 16:51:45 +0300

    refactor app.js

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -10,18 +10,18 @@ const movie1 = {
   }
   
 
-const MovieCard = ({ movie1 }) => {
+const MovieCard = ({ movie }) => {
     return (
         <div className='movie'>
-            <div>{movie1.Year}</div>
+            <div>{movie.Year}</div>
 
             <div>
-              <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.title}/>
+              <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.title}/>
             </div>
 
             <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
             </div>
           </div>
     );
